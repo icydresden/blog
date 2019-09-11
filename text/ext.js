379b626968ba61ab1edded6ca6785f97c2623273
icydresden <icydresden@gmail.com>
2019-09-11 17:57:33 +0200

    add sort function

--- text/ext.js
+++ text/ext.js
@@ -2,3 +2,11 @@ $(document).ready(function(){
     $(".row").css("color","blue");
     $("cat").addClass("animated bounce");
     });
+
+var points = [40, 100, 1, 5, 25, 10];
+$(document).getElementById("demo").innerHTML = points;
+
+function myFunction() {
+  points.sort(function(a, b){return a-b});
+  document.getElementById("demo").innerHTML = points;
+}
